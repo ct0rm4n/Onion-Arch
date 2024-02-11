72fc5ded0e1d07e45399d87020732a1019fa322c
ct0rm4n <torman@outlook.com>
2024-02-11 11:46:51 -0300

    feat - currency code api

---
+++ Infra.Service/Services/Misc/CurrencyServices.cs
@@ -0,0 +1,20 @@
+﻿using RestSharp;
+namespace Service.Application.Services.Misc
+{
+    public class CurrencyServices
+    {
+        //USD-BRL,EUR-BRL,BTC-BRL
+        public async Task GetCurrencyBRL(string[] currencyName)
+        {
+            var options = new RestClientOptions("https://economia.awesomeapi.com.br")
+            {
+                MaxTimeout = -1,
+            };
+            var client = new RestClient(options);
+            var request = new RestRequest($"/last/{string.Join(",", currencyName)}", Method.Get);
+            RestResponse response = await client.ExecuteAsync(request);
+            Console.WriteLine(response.Content);
+        }
+
+    }
+}
